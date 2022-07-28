112a3149afb4a26515b382d89e3d070f400ba6e6
Gabriel Batista <gabrielfrancisco.12@hotmail.com>
2022-07-28 15:36:43 -0300

    Status iniciais

--- SheetPlayer.py
+++ SheetPlayer.py
@@ -3,14 +3,19 @@ class Ficha:
         self.nome_personagem = nome_personagem
         self.nome_jogador = nome_jogador
         self.classe = classe
-        self.subclasse = subclasse
         self.raca = raca
+        self.subclasse = subclasse
 
     def mostra(self):
-        return (f'O nome do player é {self.nome_jogador} e seu personagem é o/a {self.nome_personagem} da raça {self.raca}.'
-                f'\nEle é da classe {self.classe} e subclasse {self.subclasse}')
+        a = (f'O nome do player é {self.nome_jogador} e seu personagem é o/a {self.nome_personagem} da raça {self.raca}.'
+                f'\nEle é da classe {self.classe} ')
+        if self.subclasse != "NoN":
+            b = (f'e subclasse {self.subclasse}')
+            print(a + b)
+        else:
+            print(a)
 
 
-fology = Ficha('Fology', 'Gabriel', 'Ladino', "Elfo")
+fology = Ficha('Fology', 'Gabriel', 'Ladino', "Elfo", "Guerreiro")
 
-print(fology.mostra())
+fology.mostra()
